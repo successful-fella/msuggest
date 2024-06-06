ca3ff2aa5473a1571a132c9e79a14225d96ef617
successful_fella <preetjuneja@zingalbox.com>
2024-06-07 00:04:00 +0530

    Fix show type issue

--- src/app/daily-movies.tsx
+++ src/app/daily-movies.tsx
@@ -36,7 +36,7 @@ export function DailyMovies() {
 	const [country, setCountry] = useState('');
 	const [movies, setMovies] = useState([]);
 	const [movieGenres, setMovieGenres] = useState([]);
-	const [showType, setShowType] = useState(Math.random() < 0.5 ? 'movie' : 'tv')
+	const [showType, setShowType] = useState(Math.random() < 0.5 ? 'movies' : 'tv')
 
 	useEffect(() => {
 		setIsClient(true);
