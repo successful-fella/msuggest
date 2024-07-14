9cf9f110178ef8b99c38ac1e01f32787a6abdcf7
rohit <rst6471@gmail.com>
2024-07-14 16:06:44 +0530

    Added more popover option

--- src/components/movie-card.tsx
+++ src/components/movie-card.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from "next/image";
+import {
+	Popover,
+	PopoverHandler,
+	PopoverContent
+  } from "@material-tailwind/react";
 
 import {
 	Typography,
@@ -23,39 +28,57 @@ export function MovieCard({
 	overview,
 }: MovieCardProps) {
 	return (
-		<Card color="transparent" shadow={false}>
-			<CardHeader color="gray" floated={false} className="mx-0 mt-0 mb-6">
-				<Image
+		(posterPath && overview) ? 
+			<Card color="transparent" shadow={false}>
+				<CardHeader color="gray" floated={false} className="mx-0 mt-0 mb-6">
+					<Image
 					width={768}
 					height={768}
-					src={`https://image.tmdb.org/t/p/w500${posterPath}`}
-					alt={title}
+						src={`https://image.tmdb.org/t/p/w500${posterPath}`}
+						alt={title}
 					className="h-full w-full scale-[1.1] object-cover object-center"
-				/>
-			</CardHeader>
-			<CardBody className="p-0">
-				<Typography color="blue" className="mb-2 text-xs !font-semibold">
-					{genreNames.map((genre, i) => (
-						<span key={i} style={{ textTransform: "capitalize" }}>
-							{genre}
-							{i < genreNames.length - 1 && ", "}
-						</span>
-					))}
-				</Typography>
-				<a href="#">
-					<Typography
-						variant="h5"
-						color="blue-gray"
-						className="mb-3 font-bold normal-case xl:w-64"
-					>
-						{title}
+					/>
+				</CardHeader>
+				<CardBody className="p-0">
+					<Typography color="blue" className="mb-2 text-xs !font-semibold">
+						{genreNames.map((genre, i) => (
+							<span key={i} style={{ textTransform: "capitalize" }}>
+								{genre}
+								{i < genreNames.length - 1 && ", "}
+							</span>
+						))}
 					</Typography>
-				</a>
-				<Typography className="mb-4 font-normal !text-gray-500">
-					{overview}
-				</Typography>
-			</CardBody>
-		</Card>
+					<a href="#">
+						<Typography
+							variant="h5"
+							color="blue-gray"
+							className="mb-3 font-bold normal-case xl:w-64"
+						>
+							{title}
+						</Typography>
+					</a>
+					<Typography className="mb-4 font-normal !text-gray-500">
+						{overview.length > 199 ? (
+							<span> {overview.substring(0,199).trim() + "..."}
+							<Popover
+								animate={{
+								mount: { scale: 1, y: 0 },
+								unmount: { scale: 0, y: 25 },
+								}}
+						  	>
+							<PopoverHandler>
+								<span className='text-blue-500 cursor-pointer'> more</span>
+							</PopoverHandler>
+							<PopoverContent className='tooltip'>
+							  {overview}
+							</PopoverContent>
+						  </Popover></span>
+						) : (
+							overview
+						)}
+					</Typography>
+				</CardBody>
+			</Card> : ""
 	);
 }
 export default MovieCard
